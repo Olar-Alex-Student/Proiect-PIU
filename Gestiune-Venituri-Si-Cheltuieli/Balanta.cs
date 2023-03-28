bb59e999731197b2f22d515c6efde2a45f44ed9b
Olar-Alex-Student <117835182+Olar-Alex-Student@users.noreply.github.com>
2023-03-28 21:33:40 +0300

    Indeplinit cerinte Laboratorului 2

---
+++ Gestiune-Venituri-Si-Cheltuieli/Balanta.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gestiune_Venituri_Si_Cheltuieli
+{
+    internal class Balanta
+    {
+        private float suma;
+        public Balanta()
+        {
+            suma = 0;
+        }
+
+        public Balanta(float _sum)
+        {
+            suma = _sum;
+        }
+
+        void addBalanta(float _add)
+        {
+            suma += _add;
+        }
+
+        void subBalanta(float _sub)
+        {
+            suma -= _sub;
+        }
+
+        public float retBalanta()
+        {
+            return suma;
+        }
+    }
+}
