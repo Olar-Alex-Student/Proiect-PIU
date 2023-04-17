a1681d59a4980e9952f6be3db07e7551b66b1ed7
Olar-Alex-Student <117835182+Olar-Alex-Student@users.noreply.github.com>
2023-04-17 19:51:50 +0300

    Creat Form

---
+++ InterfataUtilizator_WindowsForms/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace InterfataUtilizator_WindowsForms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTitlu = new System.Windows.Forms.Label();
+            this.lblNume = new System.Windows.Forms.Label();
+            this.lblSuma = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblTitlu
+            // 
+            this.lblTitlu.AutoSize = true;
+            this.lblTitlu.Location = new System.Drawing.Point(454, 37);
+            this.lblTitlu.Name = "lblTitlu";
+            this.lblTitlu.Size = new System.Drawing.Size(37, 13);
+            this.lblTitlu.TabIndex = 0;
+            this.lblTitlu.Text = "lblTitlu";
+            this.lblTitlu.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lblNume
+            // 
+            this.lblNume.AutoSize = true;
+            this.lblNume.Location = new System.Drawing.Point(267, 111);
+            this.lblNume.Name = "lblNume";
+            this.lblNume.Size = new System.Drawing.Size(45, 13);
+            this.lblNume.TabIndex = 1;
+            this.lblNume.Text = "lblNume";
+            this.lblNume.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // lblSuma
+            // 
+            this.lblSuma.AutoSize = true;
+            this.lblSuma.Location = new System.Drawing.Point(644, 111);
+            this.lblSuma.Name = "lblSuma";
+            this.lblSuma.Size = new System.Drawing.Size(44, 13);
+            this.lblSuma.TabIndex = 2;
+            this.lblSuma.Text = "lblSuma";
+            this.lblSuma.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(923, 553);
+            this.Controls.Add(this.lblSuma);
+            this.Controls.Add(this.lblNume);
+            this.Controls.Add(this.lblTitlu);
+            this.Name = "Form1";
+            this.Text = "Program";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTitlu;
+        private System.Windows.Forms.Label lblNume;
+        private System.Windows.Forms.Label lblSuma;
+    }
+}
+
---
+++ InterfataUtilizator_WindowsForms/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Nivel_Stocare_Date;
+using Cont_Utilizator;
+using System.IO;
+using System.Configuration;
+
+namespace InterfataUtilizator_WindowsForms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            string numeFisier = ConfigurationManager.AppSettings["NumeFisier"];
+            string locatieFisierSolutie =
+            Directory.GetParent(System.IO.Directory.GetCurrentDirectory()).Parent.Parent.FullName;
+            // setare locatie fisier in directorul corespunzator solutiei
+            // astfel incat datele din fisier sa poata fi utilizate si de alte proiecte
+            string caleCompletaFisier = locatieFisierSolutie + "\\" + numeFisier;
+            AdministrareVenit_FisierText adminConturi = new
+            AdministrareVenit_FisierText(caleCompletaFisier);
+            int nrConturi = 0;
+            Cont[] conturi = adminConturi.GetConturi(out nrConturi);
+            lblNume.Text = "Merge";
+            lblSuma.Text = "Merge";
+            lblTitlu.Text = "Merge";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
